e53ab7946ae1767ca2245b0df09a1d8b445a83e9
vixxjy <ajorvictor48@gmail.com>
2023-01-14 16:39:46 +0100

    git init

--- src/api.js
+++ src/api.js
@@ -1,4 +1,4 @@
-export const  token = (new URLSearchParams(window.location.search)).get("jwt")
-export const url = '/api/'
-//export const url =  'http://localhost:8282/api/';
-//export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjUzNjAyMDEyfQ.YF1f7dMVLwUscG8O6B6lV29KdUNhvoYBJOU1LcbJ1n5PR8x7ZkLMQICrImhUKwyJ3TBYj6V0_alWyY3DkIXUDQ'
+//export const  token = (new URLSearchParams(window.location.search)).get("jwt")
+//export const url = '/api/'
+export const url =  'http://localhost:8282/api/';
+export const  token = 'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjczNzUyMjY4fQ.m3AnRM1-hooaYe_hCWStR24BccvaNEuuRgBWBj78dSdvSeLfWvzmMWc8JSL39K-EjzPtiFzAWu2eEKm2sdijHA'
