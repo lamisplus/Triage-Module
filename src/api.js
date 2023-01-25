81006171f5caf22b87f7618de447b32747d7fff7
vixxjy <ajorvictor48@gmail.com>
2023-01-25 15:28:07 +0100

    Validations re-visited.

--- src/api.js
+++ src/api.js
@@ -4,9 +4,9 @@ export const url = '/api/v1/'
 
 /*
 export const url =  'http://localhost:8282/api/v1/';
-export const  token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc0NTk4OTI5fQ.uHUWH7_g30qsSaKsZf82gBlN5dAcrVQNIUzXbog1xAtWFfCb3BkyaweTPG1wVZ_dav6YP_A-blXTj_gwvhUcaQ"
-
+export const  token = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNjc0NjkyMTM2fQ.HUWkIfJCXDdMJhPZcr4hdiNM772nbux1XEMjeHqq5kuQ73V6hY29f4nbUWLY3b3j9Pn5wQac__q_uyqE_KPWJA"
 */
+
 
 
 
