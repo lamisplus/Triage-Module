6f6d7cc95e2b1ee2e44c242b76ba37d2acd912fc
vixxjy <ajorvictor48@gmail.com>
2024-03-19 11:57:59 +0100

    [triage] github link added

--- src/api.js
+++ src/api.js
@@ -1,8 +1,9 @@
-/*
+
 export const token = new URLSearchParams(window.location.search).get("jwt");
 export const url = "/api/v1/";
-*/
 
+/*
 export const url = "http://localhost:8383/api/v1/";
 export const token =
   "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJndWVzdEBsYW1pc3BsdXMub3JnIiwiYXV0aCI6IlN1cGVyIEFkbWluIiwibmFtZSI6Ikd1ZXN0IEd1ZXN0IiwiZXhwIjoxNzA3NzAzNjMyfQ.OUjt59eAW51I3z8jUqtTbzcNTZumUhq1wWhYqCDdPZab1vkR_tL_AAopjLPo8tO1xSGIdgzCnho3lJXtQ-CECQ";
+*/
